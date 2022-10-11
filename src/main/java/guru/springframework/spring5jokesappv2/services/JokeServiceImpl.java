df5e0e42a9b1f6254972047ea2169d1aa257d9e3
melifaroo <den.volgin@gmail.com>
2022-10-12 02:19:48 +0500

    implement joke service

--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -1,11 +1,22 @@
 package guru.springframework.spring5jokesappv2.services;
 
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
 public class JokeServiceImpl implements JokeService {
+
+    private guru.springframework.norris.chuck.ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
 
     @Override
     public String getJoke() {
         // TODO Auto-generated method stub
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
     
 }
