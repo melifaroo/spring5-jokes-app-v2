5587aa4c3d7e6eefd9c6476cf984a0a89974cf80
melifaroo <den.volgin@gmail.com>
2022-10-12 02:12:58 +0500

    fixing pom. adding empty joke service

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5jokesappv2.services;
+
+public class JokeServiceImpl implements JokeService {
+
+    @Override
+    public String getJoke() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
