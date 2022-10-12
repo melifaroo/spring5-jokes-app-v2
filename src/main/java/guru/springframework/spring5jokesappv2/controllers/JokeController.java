348be6daf4e18cf10130151b35693abfe5310854
melifaroo <den.volgin@gmail.com>
2022-10-12 13:48:01 +0500

    minor corrections to controller

--- src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5jokesappv2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,13 @@ import guru.springframework.spring5jokesappv2.services.JokeService;
 @Controller
 public class JokeController {
 
-    private JokeService jokeService;
+    private final JokeService jokeService;
 
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;        
     }
 
-    @RequestMapping("/")
+    @RequestMapping({"/", ""})
     public String getJoke(Model model){
         model.addAttribute( "joke", jokeService.getJoke() );
         return "jokes/index";
